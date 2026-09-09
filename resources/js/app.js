0ce51d3d921ca7d1f8a133865cb946c374ae846d
Costa <costa@kastra.id>
2026-09-09 18:05:30 +0700

    Fix: Setup Ziggy correctly via npm package ziggy-js

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,8 +16,9 @@ createInertiaApp({
     let runtime = null
 
     if (!isLanding) {
-      const [motion, toast, axiosModule, , toastNotifications] = await Promise.all([
+      const [motion, ziggy, toast, axiosModule, , toastNotifications] = await Promise.all([
         import('@vueuse/motion'),
+        import('../../vendor/tightenco/ziggy'),
         import('vue-toastification'),
         import('axios'),
         import('vue-toastification/dist/index.css'),
@@ -25,7 +26,7 @@ createInertiaApp({
       ])
 
       ToastNotifications = toastNotifications.default
-      runtime = { motion, toast, axios: axiosModule.default }
+      runtime = { motion, ziggy, toast, axios: axiosModule.default }
     }
 
     const vueApp = createApp({
@@ -35,12 +36,13 @@ createInertiaApp({
 
     if (runtime) {
       const { MotionPlugin } = runtime.motion
+      const { ZiggyVue } = runtime.ziggy
       const { default: Toast, POSITION } = runtime.toast
 
       window.axios = runtime.axios
       window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-      vueApp.use(MotionPlugin).use(Toast, {
+      vueApp.use(MotionPlugin).use(ZiggyVue).use(Toast, {
         position: POSITION.TOP_RIGHT,
         timeout: 4500,
         closeOnClick: true,
