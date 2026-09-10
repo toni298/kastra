28f0525f9472d3b47c1085e45127be127a75d45b
Costa <costa@kastra.id>
2026-09-10 07:31:08 +0700

    Fix: Update ziggy import in ssr.js

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -4,7 +4,7 @@ import { renderToString } from '@vue/server-renderer'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createSSRApp, h } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy'
+import { ZiggyVue } from 'ziggy-js'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
